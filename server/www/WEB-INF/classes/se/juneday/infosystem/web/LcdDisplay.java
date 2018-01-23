70d0027e3c81c3fc3b3f145d68629f876fcc8ddd
Henrik Sandklef <hesa@sandklef.com>
2018-01-23 23:00:30 +0100

    better param handling

--- server/www/WEB-INF/classes/se/juneday/infosystem/web/LcdDisplay.java
+++ server/www/WEB-INF/classes/se/juneday/infosystem/web/LcdDisplay.java
@@ -14,53 +14,76 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class LcdDisplay  extends HttpServlet {
 
-  private String lcdText;
-  private InfoStore infoStore;
+    private InfoStore infoStore;
   
-  public LcdDisplay(){
-    lcdText = LcdDisplay.class.getSimpleName();
-    infoStore = InfoStoreFactory.getInstance();
-    infoStore.setMessage(new Message(lcdText));
-  }
+    public LcdDisplay(){
+	infoStore = InfoStoreFactory.getInstance();
+	infoStore.setMessage(new Message(LcdDisplay.class.getSimpleName()));
+    }
 
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-    request.setCharacterEncoding(UTF_8.name());
-    response.setContentType("text/html;charset="+UTF_8.name());
-
-    String format = request.getParameter("format");
-    String write = request.getParameter("write");
-
-    PrintWriter out =
-      new PrintWriter(new OutputStreamWriter(response.getOutputStream(),
-                                             UTF_8), true);
-    String when = "" + new Date();
-
-    if ( write != null ) {
-      infoStore.setMessage(new Message(write));
-    } else if((format == null) || format.equals("html") ){
-      out.println("<!DOCTYPE html>");
-      out.println("<html lang=\"en\">");
-      out.println("<head><title>Juneday's Information System, well it's a bogus</title></head>");
-      out.println("<body>");
-      out.println("<h1>Text on LCD: " + lcdText + "</h1>");
-      out.println("</body>");
-      out.println("</html>");
-      lcdText += " ll";
-      infoStore.setMessage(new Message(lcdText));
-    }else if(format.equals("json")){
-      System.out.println(" store: " + infoStore);
-      System.out.println(" message: " + infoStore.message());
-      System.out.println(" formatted: " + infoStore.formatted());
-      out.println(infoStore.formatted());
-    }else{
-      System.out.println(when + " " + format + " not supported");
+    private void htmlBegin(PrintWriter out) {
+	out.println("<!DOCTYPE html>");
+	out.println("<html lang=\"en\">");
+	out.println("<head><title>Juneday's Information System, well it's a bogus</title></head>");
+	out.println("<body>");
     }
     
+    private void htmlEnd(PrintWriter out) {
+	out.println("</body>");
+	out.println("</html>");
+    }
     
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+	request.setCharacterEncoding(UTF_8.name());
+	response.setContentType("text/html;charset="+UTF_8.name());
 
-  }
+	String format = request.getParameter("format");
+	String write = request.getParameter("write");
+	int nrParameters = request.getParameterMap().entrySet().size();
 
+	
+	PrintWriter out =
+	    new PrintWriter(new OutputStreamWriter(response.getOutputStream(),
+						   UTF_8), true);
+	String when = "" + new Date();
+
+	// System.out.println(" let's parse");
+	if ( write != null ) {
+	    htmlBegin(out);
+	    if (write.length()<32) {
+		Message m = new Message(write);
+		infoStore.setMessage(m);
+		out.println("<br><h1>Text has been updated .... </h1>");
+	    }
+	    out.println("<h1>Text on LCD is now: " +  infoStore.message() + "</h1>");
+	    htmlEnd(out);
+	    return;
+	}
+	if (nrParameters==0) {
+	    htmlBegin(out);
+	    out.println("<h1>Text on LCD: " +  infoStore.message() + "</h1>");
+	    htmlEnd(out);
+	} else if ( format != null){
+	    if (format.equals("html")) {
+		htmlBegin(out);
+		out.println("<h1>Text on LCD: " +  infoStore.message() + "</h1>");
+		htmlEnd(out);
+	    } else if(format.equals("json")){
+		out.println(infoStore.formatted());
+	    } else {
+		htmlBegin(out);
+		out.println("<h1>Text on LCD: " +  infoStore.message() + "</h1>");
+		out.println("<h1>Format uknown: " + format + "</h1>");
+		htmlEnd(out);
+	    }
+	} else {
+	    htmlBegin(out);
+	    out.println("<br><h1>Text has NOT been updated, bad parameter </h1>");
+	    htmlEnd(out);	    
+	}
+	
+    }
+    
 }
